ab9fe9b5c0deb49f8f958ec44d2afd548f08fd0c
chris <chrisj21283@gmail.com>
2023-09-22 13:25:37 -0400

    made question variables and starting h1 value

---
+++ script.js
@@ -0,0 +1,18 @@
+var question = document.querySelector("#intro");
+
+question.textContent = "Coding Quiz Challenge";
+
+var q1 = "commonly used dats types DO Not Include:"
+// 1 .strings 2.booleans 3.alerts 4.numbers
+var q2 = "The condition in an if / else statement is enclosed with _____"
+//1.quotes 2.curly brackets 3.parenthesis 4.square brackets
+var q3 = "Arrays in JavaScript can be used to store _____"
+// 1.numbers and strings 2.other arrays 3.booleans 4.all of the above
+var q4 = "String values must be enclosed within _____ when being assigned to variables."
+// 1.commas 2. curly brackets 3.quotes 4.parenthesis
+var q5 = " A very useful tool used during development and debugging for printing content to the debugger is :"
+// 1 JavaScript 2 . terminal/bash 3.for loops 4.console.log
+
+
+// "All done!"   your final score is XX.  Enter initials-input box  submit button
+
