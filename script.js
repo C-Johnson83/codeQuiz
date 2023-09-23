bcf61da2c2d8cbde622dc69736607e9087d4d505
chris <chrisj21283@gmail.com>
2023-09-23 17:10:55 -0400

    added buttons after start

--- script.js
+++ script.js
@@ -2,108 +2,108 @@
 var qstn = document.querySelector("#intro");
 var message = document.querySelector("#message");
 var subButton = document.querySelector("#start");
-var qBtn1 = document.querySelector("#b1");
-var qBtn2 = document.querySelector("#b2");
-var qBtn3 = document.querySelector("#b3");
-var qBtn4 = document.querySelector("#b4");
+var myDiv = document.getElementById("form");
+var qBtn1 = document.createElement('button')
+qBtn1.setAttribute("id", "b1")
+var qBtn2 = document.createElement('button')
+qBtn2.setAttribute("id", "b2")
+var qBtn3 = document.createElement('button')
+qBtn3.setAttribute("id", "b3")
+var qBtn4 = document.createElement('button')
+qBtn4.setAttribute("id", "b4")
+var i = 0;
+var score = 0;
 // Set question and answer array
 
-var questionArray= [
-{
-    question: "Commonly used data types DO Not Include:",
-    answers: {
-        1: "strings",
-        2: "booleans",
-        3: "alerts",
-        4: "numbers"
+var questionArray = [
+    {
+        question: "Commonly used data types DO Not Include:",
+        answers: {
+            1: "strings",
+            2: "booleans",
+            3: "alerts",
+            4: "numbers"
+        },
+        correctAnswer: '3'
     },
-    correctAnswer: '3'
-},
-{
-    question: "The condition in an if / else statement is enclosed with _____",
-    answers: {
-        1: "quotes",
-        2: "parenthesis",
-        3: "curly brackets",
-        4: "square brackets"
+    {
+        question: "The condition in an if / else statement is enclosed with _____",
+        answers: {
+            1: "quotes",
+            2: "parenthesis",
+            3: "curly brackets",
+            4: "square brackets"
+        },
+        correctAnswer: '2'
     },
-    correctAnswer: '2'
-},
-{
-    question: "Arrays in JavaScript can be used to store _____",
-    answers: {
-        1: "numbers and strings",
-        2: "other arrays",
-        3: "booleans",
-        4: "all of the above"
+    {
+        question: "Arrays in JavaScript can be used to store _____",
+        answers: {
+            1: "numbers and strings",
+            2: "other arrays",
+            3: "booleans",
+            4: "all of the above"
+        },
+        correctAnswer: '4'
     },
-    correctAnswer: '4'
-},
-{
-    question: "String values must be enclosed within _____ when being assigned to variables.",
-    answers: {
-        1: "commas",
-        2: "curly brackets",
-        3: "quotes",
-        4: "parenthesis"
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        answers: {
+            1: "commas",
+            2: "curly brackets",
+            3: "quotes",
+            4: "parenthesis"
+        },
+        correctAnswer: '3'
     },
-    correctAnswer: '3'
-},
-{
-    question: " A very useful tool used during development and debugging for printing content to the debugger is :",
-    answers: {
-        1: "console.log",
-        2: "terminal/bash",
-        3: "for loops",
-        4: "JavaScript"
+    {
+        question: " A very useful tool used during development and debugging for printing content to the debugger is :",
+        answers: {
+            1: "console.log",
+            2: "terminal/bash",
+            3: "for loops",
+            4: "JavaScript"
+        },
+        correctAnswer: '1'
     },
-    correctAnswer: '1'
-},
-]
-console.log(questionArray)
-
-var btnwds = [q1Ops, q2Ops, q3Ops, q4Ops, q5Ops];
-var i=0;
-var score = 0;
+];
 // set starting values for text elements
 qstn.textContent = "Coding Quiz Challenge";
 message.textContent = "Try to answer the following code related questions within the time limit.\n\nKeep in mind that incorrect answers will penalize your score/time by 10 seconds";
-qBtn1.textContent = "";
-qBtn2.textContent = "";
-qBtn3.textContent = "";
-qBtn4.textContent = "";
-subButton.addEventListener("click", question);
+subButton.addEventListener("click", questionTime);
 
-function question(event) {
+
+
+function questionTime(event) {
+    myDiv.appendChild(qBtn1);
+    myDiv.appendChild(qBtn2);
+    myDiv.appendChild(qBtn3);
+    myDiv.appendChild(qBtn4);
+    event.preventDefault();
 
     // Prevent default action
-    event.preventDefault();
+
     console.log(event);
     // subButton.setAttribu"hidden", '')
-    qstn.textContent = qArray[i];
+    qstn.textContent = questionArray[i].question;
     message.textContent = "";
-    qBtn1.textContent = btnwds[i][0];
-    qBtn2.textContent = btnwds[i][1];
-    qBtn3.textContent = btnwds[i][2];
-    qBtn4.textContent = btnwds[i][3];
+    qBtn1.textContent = questionArray[i].answers[1];
+    qBtn2.textContent = questionArray[i].answers[2];
+    qBtn3.textContent = questionArray[i].answers[3];
+    qBtn4.textContent = questionArray[i].answers[4];
     console.log(i)
     i++;
 
 
-    // if (i == 5) {
+    if (i == 5) {
 
- 
-    //       qstn.textContent = "All done!";
-    //     message.textContent = "your final score is " + score;
-    //     qBtn1.textContent = "";
-    //     qBtn2.textContent = "";
-    //     qBtn3.textContent = "";
-    //     qBtn4.textContent = "";
-    // }
+
+        qstn.textContent = "All done!";
+        message.textContent = "your final score is " + score;
+        myDiv.removeChild(qBtn1);
+        myDiv.removeChild(qBtn2);
+        myDiv.removeChild(qBtn3);
+        myDiv.removeChild(qBtn4);
+    }
 }
 ;
-// Ending message
-// .
-//age "All done!"   
-// your final score is XX.  
-// Enter initials input-box  submit button
